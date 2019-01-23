4662866f8936a9a7df26905801172933d17abe6e
MaximVV <laul@bk.ru>
2019-01-24 00:19:10 +0300

    addTestReader

---
+++ src/test/java/com/vasiliska/students/dao/DataReaderTest.java
@@ -0,0 +1,24 @@
+package com.vasiliska.students.dao;
+
+import com.vasiliska.students.service.Question;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+//@DisplayName()
+public class DataReaderTest {
+
+    @Test
+    public void readData() {
+
+        DataReader dataReader = new DataReader();
+        dataReader.setFileName("quizTestUS.csv");
+        List<Question> listQuestion = dataReader.readData();
+        int sizeList =  listQuestion.size();
+
+        Assert.assertEquals(6, sizeList);
+    }
+}
